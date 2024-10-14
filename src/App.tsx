71645ffec27011a8ac3b5d4fdcc886d564428a4c
sutigit <panyanilben@gmail.com>
2024-10-14 17:50:21 +0300

    add ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <main>
       <h1>Hello DockerHub</h1>
-      <p>from vite; by sutigit</p>
+      <p>from <a href='https://vite.dev/'>vite</a>; in <a href='https://render.com/'>render</a>; by <a href='https://github.com/sutigit/first-dockerhub-publish'>sutigit</a></p>
     </main>
   )
 }
